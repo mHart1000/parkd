b20716bdecde874bdb602bea219b23ad0a1b7a5d
Matthew Hart <matthewhart1000@gmail.com>
2025-01-16 23:10:54 -0800

    fix cors error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :set_cors_headers
+
+  private
+
+  def set_cors_headers
+    response.set_header('Access-Control-Allow-Credentials', 'true')
+  end
 end
