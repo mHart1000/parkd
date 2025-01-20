45594ea474b825659d076e71165590e3f9b93e23
Matthew Hart <matthewhart1000@gmail.com>
2025-01-20 00:53:46 -0800

    adjust devise config for api only

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,6 +2,18 @@ module Api
   class SessionsController < Devise::SessionsController
     respond_to :json
 
+    def create
+      Rails.logger.info "Attempting to sign in with params: #{params[:user].inspect}"
+      user_params = params.require(:user).permit(:email, :password)
+      self.resource = warden.authenticate!(auth_options.merge(scope: :user))
+      if self.resource
+        sign_in(resource_name, resource)
+        respond_with(resource, location: after_sign_in_path_for(resource))
+      else
+        render json: { error: 'Invalid credentials' }, status: :unauthorized
+      end
+    end
+
     private
 
     def respond_with(resource, _opts = {})
@@ -14,6 +26,10 @@ module Api
 
     def respond_to_on_destroy
       head :no_content
+    end
+
+    def auth_options
+      { scope: resource_name }
     end
   end
 end
