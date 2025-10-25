eccdb9382ceff2ad9f8ae2fc747789e83e86aa45
Matthew Hart <mhart1000@users.noreply.github.com>
2025-10-25 00:36:12 -0700

    add user serializer

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,10 +3,14 @@ module Api
     def create
       user = User.new(user_params)
       if user.save
-        render json: { user: user }, status: :created
+        render json: user, serializer: UserSerializer, status: :created
       else
         render_error(user.errors.full_messages.join(", "), :unprocessable_entity)
       end
+    end
+
+    def show
+      render json: current_user, serializer: UserSerializer
     end
 
     private
--- parkd-client/src/pages/ProfilePage.vue
+++ parkd-client/src/pages/ProfilePage.vue
@@ -2,49 +2,34 @@
   <q-page class="q-pa-md">
     <q-card>
       <q-card-section>
-        <div class="text-h6">Your Profile</div>
+        <div class="text-h6">Profile</div>
       </q-card-section>
       <q-card-section>
-        <q-input v-model="profile.first_name" label="First Name" />
-        <q-input v-model="profile.last_name" label="Last Name" />
-        <q-input v-model="profile.bio" label="Bio" type="textarea" />
-        <q-input v-model="profile.avatar_url" label="Avatar URL" />
+        <q-input v-model="profile.name" label="Name" :disable />
+        <q-input v-model="profile.email" label="Email" :disable />
       </q-card-section>
-      <q-card-actions align="right">
-        <q-btn color="primary" label="Save" @click="updateProfile" />
-      </q-card-actions>
     </q-card>
   </q-page>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue'
-import { useQuasar } from 'quasar'
-import axios from 'axios'
+import { api } from 'src/boot/axios'
 
 export default {
   name: 'ProfilePage',
   setup () {
-    const $q = useQuasar()
-    const profile = ref({ first_name: '', last_name: '', bio: '', avatar_url: '' })
+    const profile = ref({ name: '', email: '' })
 
     const fetchProfile = async () => {
-      const res = await axios.get('/api/profile')
+      const res = await api.get('/user/')
+      console.log('Profile data fetched:', res.data)
       profile.value = res.data
-    }
-
-    const updateProfile = async () => {
-      try {
-        await axios.put('/api/profile', { profile: profile.value })
-        $q.notify({ type: 'positive', message: 'Profile updated!' })
-      } catch (err) {
-        $q.notify({ type: 'negative', message: 'Error saving profile' })
-      }
     }
 
     onMounted(fetchProfile)
 
-    return { profile, updateProfile }
+    return { profile }
   }
 }
 </script>
