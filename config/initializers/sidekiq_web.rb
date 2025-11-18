1a5a226af89263098da3190d8447c98aefc0a406
Matthew Hart <mhart1000@users.noreply.github.com>
2025-11-18 01:26:41 -0800

    rubocop

--- config/initializers/sidekiq_web.rb
+++ config/initializers/sidekiq_web.rb
@@ -1,5 +1,5 @@
 # Enable sessions (cookies) only for Sidekiq::Web
-Rails.application.config.session_store :cookie_store, key: '_parkd_session'
+Rails.application.config.session_store :cookie_store, key: "_parkd_session"
 
 Rails.application.config.middleware.use ActionDispatch::Cookies
 Rails.application.config.middleware.use ActionDispatch::Session::CookieStore, Rails.application.config.session_options
