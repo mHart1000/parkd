63e1a3daacc86479a63afd5834267c37825d4701
Matthew Hart <mhart1000@users.noreply.github.com>
2025-12-19 17:02:08 -0800

    auth user & link to subscribe create

--- app/controllers/api/push_subscriptions_controller.rb
+++ app/controllers/api/push_subscriptions_controller.rb
@@ -1,18 +1,24 @@
 class Api::PushSubscriptionsController < ApplicationController
   def create
-    data = params[:push_subscription] || params
+    data = params[:push_subscription]
 
     endpoint = data[:endpoint]
-    keys = data[:keys] || params[:keys]
+    keys = params[:keys]
+    return head :bad_request unless endpoint && keys
 
-    subscription = PushSubscription.find_or_initialize_by(endpoint: endpoint)
+    user = current_user
+    return head :unauthorized unless user
+
+    subscription = user.push_subscriptions.find_or_initialize_by(endpoint: endpoint)
 
     subscription.p256dh_key = keys[:p256dh]
     subscription.auth_key = keys[:auth]
 
-    subscription.save!
-
-    head :ok
+    if subscription.save
+      head :ok
+    else
+      render json: { errors: subscription.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def index
