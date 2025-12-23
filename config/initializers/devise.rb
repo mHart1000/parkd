9fc5cee82449c711c0bce0256b39d2fea735a822
Matthew Hart <mhart1000@users.noreply.github.com>
2025-12-22 23:55:56 -0800

    skip params_auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -97,7 +97,7 @@ Devise.setup do |config|
   # Notice that if you are skipping storage for all authentication paths, you
   # may want to disable generating routes to Devise's sessions controller by
   # passing skip: :sessions to `devise_for` in your config/routes.rb
-  config.skip_session_storage = [ :http_auth ]
+  config.skip_session_storage = [ :http_auth, :params_auth ]
 
   # By default, Devise cleans up the CSRF token on authentication to
   # avoid CSRF token fixation attacks. This means that, when using AJAX
@@ -316,8 +316,6 @@ Devise.setup do |config|
     jwt.revocation_requests = [ [ "DELETE", %r{^/api/users/sign_out$} ] ]
     jwt.expiration_time = 1.days.to_i
   end
-
-  config.skip_session_storage = [ :http_auth ]
 
   Warden::Manager.after_authentication do |user, auth, opts|
     Rails.logger.debug "Warden authenticated: #{user.inspect}"
