24c785e6b3a02ccf3310b096cc18309778c86a48
Matthew Hart <mhart1000@users.noreply.github.com>
2025-12-28 18:40:21 -0800

    remove dead registration logic

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,5 @@
 module Api
   class UsersController < ApiController
-    skip_before_action :authenticate_user!, only: [:create]
-
-    def create
-      user = User.new(user_params)
-      if user.save
-        render json: user, serializer: UserSerializer, status: :created
-      else
-        render_error(user.errors.full_messages.join(", "), :unprocessable_entity)
-      end
-    end
-
     def show
       render json: current_user, serializer: UserSerializer
     end
